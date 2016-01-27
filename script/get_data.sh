17c8c355b30e9fbc24f210b6c72d23c515d46c72
Q.Liu <xj67@whu.edu.cn>
2016-01-27 11:14:05 +0800

    Modify get_data.sh

--- script/get_data.sh
+++ script/get_data.sh
@@ -1,26 +1,25 @@
 #!/bin/bash
-#==================================================================
+# ==========================================================
+# Program Description
 #
-# Function
-#  
+# Function:
 # Download GNSS data and products
 #
 # Warning:
-#
 # We already have several GNSS data products' URL in URL_LIST.txt,
 # and we must install the toolkit called GPSTk as the same time so 
 # that we can use some tools such as "timeconvert", or you can copy
 # the executive program "timeconvert" to your user directory "/usr/
 # local/bin". 
 # When you need to segment or merge the observation file, you 
-# should prepare the teqc tool.
+# should prepare the teqc tool;You need install the CRX2RNX tool 
+# When the RINEX files need to be converted from Hatanaka compact 
+# format to standard RINEX.
 # 
 # Copyright (c)  
-#
 # Q.Liu, Wuhan University, 2015
 #
-#==================================================================
-#
+# ==========================================================
 # Modifications
 #
 # 2015/10/08
@@ -39,40 +38,45 @@
 # 2015/10/29
 # Added several functions: merge observation files; write the file 
 # name list into a text file; extend the time span when the product 
-# type is CLK, EPH and ERP and SSC.
+# type is CLK, EPH and ERP.
 #
-# 2015/11/18
-# Fix some bugs.
-#
-#==================================================================
+# ==========================================================
 
+
+# ============================
 # Print the help message
+# ============================
 Help()
 {
 	echo "get_data.sh,下载GNSS产品的bash程序。"
 	echo "用法: bash get_data.sh [起始时间][终止时间][时间间隔][机构][产品类型][URL路径]...[选项]"
-	echo "示例：bash get_data.sh -b \"2014 3 1 0 0 0\" -e \"2014 3 3 0 0 0\" -i 24 -a IGS -t OBS -s "COCO" -u \"url_list.txt\" -p \"./GNSS/products\" -d"
 	echo "参数说明："
-	echo "-h,   --help                      获取帮助信息"
-	echo "-b,   --bigining of time span     需要的产品的起始时间，格式为民用时（年 月 日 时 分 秒）"
-	echo "-e,   --ending of time span       需要的产品的终止时间，格式为民用时（年 月 日 时 分 秒）"
-	echo "-i,   --interval of time span     时间间隔（单位：小时，必须为整数） "
-	echo "-a,   --agency of GNSS product    发布GNSS产品的机构"
-	echo "                                  机构的选项有：IGS、IGR、IGU、COD、ESA、JPL、MGEX、GFZ、IGSE
-	                                        、IERS、JAXA、RTPP、GSI、SIO、IGUE、IGSS、VMF等"
-	echo "-s,   --station name or station
-                  list file            	    测站名或者测站列表文件"
-	echo "-t,   --type of product           下载的产品类型"	
-	echo "                                  产品类型选项：EPH、EPH_GLO、CLK、CLK_30S、CLK_5S、ERP、POS、
-	                                        ZPD、TEC、DCB_P1P2、NAV、OBS、ATX_08、GRID等"
-	echo "-u,   --the path of url_list file 保存有各种产品的url列表文件的路径"
-	echo "-p,   --the data save path        下载数据保存路径"
-	echo "-l,   --the preffix of list file  保存下载数据文件名的文件前缀"
-	echo "-d,   --decompress                选择是否解压，如果带有-d选项，则解压文件；若未带有-d选项，则不解压"
-	echo "-m,   --merge                     选择是否将同一测站的观测数据进行合并"
+	echo "    -h,   --help                      获取帮助信息"
+	echo "    -b,   --bigining of time span     需要的产品的起始时间，格式为民用时（年 月 日 时 分 秒）"
+	echo "    -e,   --ending of time span       需要的产品的终止时间，格式为民用时（年 月 日 时 分 秒）"
+	echo "    -i,   --interval of time span     时间间隔（单位：小时，必须为整数） "
+	echo "    -a,   --agency of GNSS product    发布GNSS产品的机构"
+	echo "                                      机构的选项有：IGS、IGR、IGU、COD、ESA、JPL、MGEX、GFZ、IGSE
+	                                            、IERS、JAXA、RTPP、GSI、SIO、IGUE、IGSS、VMF等"
+	echo "    -s,   --station name or station   测站名或者测站列表文件
+                     list file"
+	echo "    -t,   --type of product           下载的产品类型"	
+	echo "                                      产品类型选项：EPH、EPH_GLO、CLK、CLK_30S、CLK_5S、ERP、POS、
+	                                            ZPD、TEC、DCB_P1P2、NAV、OBS、ATX_08、GRID等"
+	echo "    -u,   --the path of url_list file 保存有各种产品的url列表文件的路径"
+	echo "    -p,   --the data save path        下载数据保存路径"
+	echo "    -l,   --the preffix of list file  保存下载数据文件名的文件前缀"
+	echo "    -d,   --decompress                选择是否解压，如果带有-d选项，则解压文件；若未带有-d选项，则不解压"
+	echo "    -m,   --merge                     选择是否将同一测站的观测数据进行合并"
+
+	echo "示例：bash get_data.sh -b \"2014 3 1 0 0 0\" -e \"2014 3 3 0 0 0\" -i 24 -a IGS -t OBS -s "COCO" -u \"url_list.txt\" -p \"./GNSS/products\" -d"
+	echo "      bash get_data.sh -b \"2014 3 1 0 0 0\" -e \"2014 3 3 0 0 0\" -i 24 -a IGS -t OBS -s "COCO" -u \"url_list.txt\" -l igs1781 -p \"./GNSS/products\" -d"
+	exit 1;
 }
 
+# ============================
 # Assign the parameter with the revalent value according input data
+# ============================
 ParaAssign()
 {
 	# Assign the value for the time parameters
@@ -97,7 +101,9 @@ ParaAssign()
 	D=0	 #day of week
 }
 
+# ============================
 # Read the station list from station list file.
+# ============================
 ReadStation()
 {
 	i=0
@@ -112,14 +118,15 @@ ReadStation()
 	done
 }
 
+# ============================
 # Judge the validity of arguments
+# ============================
 Judge()
 {
 	# Judge the validity of arguments
 	if [[ $NUMARGS -le 1 ]]
 	then
-	echo "The arguments' quantity is not enough, please use \"bash get_data.sh -h\" command to get help"
-		exit 1
+		Help
 	fi
 	if [[ $segment_intv -le 0 ]]
 	then 
@@ -174,7 +181,9 @@ Judge()
 	fi
 }
 
+# ============================
 # Create the ftp address which can download GNSS data products 
+# ============================
 URLMatch()
 {
 	# Convert the lower letter to the upper case
@@ -192,9 +201,7 @@ URLMatch()
 	if [[ -z $loc ]]
 	then
 		# if can't find the str in the file
-      echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-      echo "The data type ($product_type) can't be found in ($agency) type list in URL_List.txt  !"
-      echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+		echo "所输入的机构($agency)或产品类型参数($product_type)在本列表中无效"
 		exit 1
 	fi
     
@@ -214,12 +221,21 @@ URLMatch()
 	[[ -n $w ]] && [[ $url =~ "%w" ]] && url=${url//%w/$w} 
 }
 
+# ============================
 # Convert the time to the needed format
+# ============================
 TimeConvert()
 {
-	# Use the timeconvert tool to convert julian date to 
-	# the needed time  	
+	  # Use the timeconvert tool to convert julian date to 
+	  # the needed time  	
 	gpsweek=$(timeconvert -j "$JD" -F "%F")
+	  
+	  # Judge if the timeconvert tool exists.
+   if [ $? -ne 0 ]
+	then
+		 echo "Failed: timeconvert tool doesn't exist!"
+		 echo "Please install the timeconvert tool in advance."
+	fi
 	doy=$(timeconvert -j "$JD" -F "%j")
 	dow=$(timeconvert -j "$JD" -F "%w")
 
@@ -278,7 +294,9 @@ TimeConvert()
 	w=$woy
 }
 
+# ============================
 # Convert civil time to julian date
+# ============================
 CT2JD()
 {
 	# JD:julian date
@@ -291,7 +309,9 @@ CT2JD()
 	JD=$(timeconvert -c "$m $d $Y $h:$M:$S" -F "%J")
 }
 
+# ============================
 # Convert julian date to civil time
+# ============================
 JD2CT()
 {
 	Y=$(timeconvert -j "$JD" -F "%Y")
@@ -302,7 +322,9 @@ JD2CT()
 	S=$(timeconvert -j "$JD" -F "%S")
 }
 
+# ============================
 # Encode the hour from number to alphabet, 0-a,1-b,...,23-x
+# ============================
 HourCode()
 {
 	let temp=$h+97
@@ -310,51 +332,19 @@ HourCode()
 	H=$(echo -e "\\x$temp")
 }
 
+# ============================
 # Download data and save it
+# ============================
 Download()
 {
 	# Extract data's downloading filename
 	compressed_file=$(echo $url|awk -F '/' '{print $NF}')
+	zfile="$savepath""$compressed_file"
 
 	# Wipe off the compressed file's suffix ".Z"
 	filename=$(echo $compressed_file | cut -d "." -f 1,2)
 		
 	savefile="$savepath""$filename"
-
-	if [[ -e $savefile ]]
-	then
-		echo "The file $savefile already exists!"
-	else
-		wget -c -P $savepath $url
-		if [[ ! -e $savefile ]]
-		then
-			echo "Warning! Failed when downloading the file $savefile."
-			echo "( Please confirm if this file exists on the network. ) "
-		fi
-	fi
-}
-
-# Decompress the downloaded data file
-Decompress()
-{
-	compressed_file=$(echo $url|awk -F '/' '{print $NF}')
-	   # Get the data file's suffix
-	suffix=${compressed_file##*.}
-	
-       # Judge if the savepath's last character is '/'
-	zfile="$savepath""$compressed_file"
-	   # Judge if the "compressed_file" is a file and a compressed file
-	if [ -f $zfile ]  
-	then
-		if [ $suffix = "Z" ] || [ $suffix = "gz" ]
-		then	
-         if [ ! -e $savefile ]
-         then
-			   gzip -d "$zfile"
-         fi
-		fi
-	fi
-
 	   # If the observation's format is 31(Hatanaka Compact RINEX),
 	   # we should use CRX2RNX tool to convert its format.
 	if [[ ${filename: -1} = "d" ]] && [[ $product_type = "OBS" ]]
@@ -370,22 +360,65 @@ Decompress()
 
 		savefile2="$savepath""$filename2"
 
-		if [[ -e $savefile2 ]]
+	fi
+
+	if [[ -e $savefile ]]
+	then
+		echo "The file $savefile already exists!"
+   elif [[ $product_type = "OBS"  ]] && [[ -e $savefile2 ]]
+	then
+		echo "The file $savefile2 already exists!"
+	else
+		wget -c -P $savepath $url
+	fi
+}
+
+# ============================
+# Decompress the downloaded data file
+# ============================
+Decompress()
+{
+	compressed_file=$(echo $url|awk -F '/' '{print $NF}')
+	   # Get the data file's suffix
+	suffix=${compressed_file##*.}
+	
+	   # Judge if the "compressed_file" is a file and a compressed file
+	if [ -f $zfile ] || [ -e $savefile ]
+	then
+		if [ $suffix = "Z" ] || [ $suffix = "gz" ]
+		then	
+         if [ ! -e $savefile ]
+         then
+			   gzip -d "$zfile"
+         fi
+		fi
+
+	   # If the observation's format is 31(Hatanaka Compact RINEX),
+	   # we should use CRX2RNX tool to convert its format.
+	if [[ ${filename: -1} = "d" ]] && [[ $product_type = "OBS" ]]
+	then
+		if [[ ! -e $savefile2 ]]
 		then
-			echo "The file $savefile2 already exists!"
-		else
             # Convert
 	      CRX2RNX "$savefile"
-      fi
 
+	         # Judge if the CRX2RNX tool exists.
+         if [ $? -ne 0 ]
+	      then
+		      echo "Failed: CRX2RNX tool doesn't exist!"
+		      echo "Please install the CRX2RNX tool in advance."
+	      fi
+      fi
          # Now, observation file name has already changed to '*.yyo'
       filename=$filename2;
       savefile=$savefile2
 	fi
-
+	fi
 }
 
+# ============================
 # Segment the observation file
+# ============================
 Segment()
 {
 	# "workplace" remebers current work directory
@@ -419,6 +452,13 @@ Segment()
 					echo "The file $savepath$segfilename1 already exists!"
 				else
 					teqc -st $start_time +dh $segment_intv $filename>"$segfilename1"  
+	  
+					  # Judge if the teqc tool exists.
+					if [ $? -ne 0 ]
+					then
+						echo "Failed: teqc tool doesn't exist!"
+						echo "Please install the teqc tool in advance."
+					fi
 				fi
 				# Push the segmentation file's name into the FileList array.
 				FileList[list_num]="$savepath""$file_head""$time_quantum""$file_tail"
@@ -451,8 +491,10 @@ Segment()
 	cd $workplace
 }
 
+# ============================
 # Store the downloaded file which has the same data type into a text file.
 # Observation file need having the same station.
+# ============================
 MakeList()
 {
 	# The list file gets the suffix  
@@ -504,6 +546,14 @@ MakeList()
 	then
 		rm "$project.dcb_p1c1list"
 	fi
+	if [[ $product_type = "DCB_P1P2" ]] && [[ -e "$project.dcb_p1p2list" ]]
+	then
+		rm "$project.dcb_p1p2list"
+	fi
+	if [[ $product_type = "DCB_P1P2_DAILY" ]] && [[ -e "$project.dcb_p1p2list" ]]
+	then
+		rm "$project.dcb_p1p2list"
+	fi
 	if [[ $product_type = "REC" ]] && [[ -e "$project.reclist" ]]
 	then
 		rm "$project.reclist"
@@ -545,6 +595,12 @@ MakeList()
 		elif [[ $product_type = "DCB_P1C1" ]]
 		then
 			echo $var >> "$project.dcb_p1c1list"
+		elif [[ $product_type = "DCB_P1P2" ]]
+		then
+			echo $var >> "$project.dcb_p1p2list"
+		elif [[ $product_type = "DCB_P1P2_DAILY" ]]
+		then
+			echo $var >> "$project.dcb_p1p2list"
 		elif [[ $product_type = "SSC" ]]
 		then
 			echo $var >> "$project.ssclist"
@@ -556,20 +612,22 @@ MakeList()
 	fi
 }
 
+# ============================
 # Merge the several observation which has the same station into a whole file
+# ============================
 Merge()
 {
-	# Read the observation files list
+	  # Read the observation files list
 	rnxlist=`echo ${FileList[*]}`
 
-	# Assign for the merged file.
+	  # Assign for the merged file.
 	store_file="$s"."$y""o"
 
 	unset FileList
 	declare -a FileList
 	savefile="$savepath""$store_file"
 
-	# Merge the observation files from the rnxlist.
+	  # Merge the observation files from the rnxlist.
 	if [[ -e $savefile ]]
 	then
 		echo "The file $savefile already exists!"
@@ -584,7 +642,7 @@ Merge()
 	fi
 }
 
-# Read the input arguments
+  # Read the input arguments
 while getopts "b:e:i:t:u:a:s:p:l:dmh" arg
 do
 	case $arg in
@@ -599,10 +657,9 @@ do
 		p)  savepath=$OPTARG;;
 		m)  merge=1;;
 		l)  project=$OPTARG;;
-		h)  Help
-		exit 1;;
+		h)  Help ;;
 		*)  echo "Can not find the argument "$OPTARG""
-		exit 1;;
+			 exit 1;;
 	esac
 done
 
@@ -619,7 +676,7 @@ Date2=($Y2 $m2 $d2 $h2 $M2 $S2)
 CT2JD ${Date2[@]}
 jd2=${JD%.*}
 
-# Calculate the b's julian date
+  # Calculate the b's julian date
 Date1=($Y1 $m1 $d1 $h1 $M1 $S1)
 CT2JD ${Date1[@]}
 jd1=${JD%.*}
@@ -671,51 +728,51 @@ then # When the product type is OBS and 's' displays a list file.
 	for var in ${StationList[@]}
 	do
 		JD=$JD1
-		s=$var
-		# Convert the capital letter to the lower case
-		s=$(echo $s | tr '[A-Z]' '[a-z]')  
+		  # Convert the capital letter to the lower case
+		s=$(echo $var | tr '[A-Z]' '[a-z]')  
 		for ((i=0;i<intv_num;++i))
 		do
 			TimeConvert
 			HourCode
 			URLMatch
 			Download
-			   # When the $savefile doesn't exist, jump out the loop
-			if [[ ! -e $savefile ]]
+		   if [[ -e $zfile ]] || [[ -e $savefile ]]
 			then
+				if [[ "$de" = "1" ]]
+				then 
+					Decompress
+				fi
+				if [[ $segment_intv = 24 ]] && [[ $merge = 1 ]]
+				then
+					FileList[$i]=$savefile
+				fi
+				if [[ $segment_intv = 24 ]] && [[ $merge != 1 ]]
+				then
+					FileList[$i+$j*$intv_num]=$savefile
+				fi
+				if [ $segment_intv -lt 24 ]
+				then
+					if [ $de = 1 ]
+					then
+						Segment 
+					else
+						  # If the downloaded file has not been decompressed, we 
+						  # can't segment this file.
+						echo "Please decompress the downloaded file."
+						exit 1
+					fi
+				fi
+			else
+				JD=$(awk 'BEGIN{printf("%.1f\n",'$JD+$intv/24')}')
 				continue
 			fi
-			if [ "$de" = "1" ]
-			then 
-				Decompress
-			fi
-			if [[ $segment_intv = 24 ]] && [[ $merge = 1 ]]
-			then
-				FileList[$i]=$savefile
-			fi
-			if [[ $segment_intv = 24 ]] && [[ $merge != 1 ]]
-			then
-				FileList[$i+$j*$intv_num]=$savefile
-			fi
-			if [ $segment_intv -lt 24 ]
-			then
-				if [ $de = 1 ]
-				then
-					Segment 
-				else
-					# If the downloaded file has not been decompressed, we 
-					# can't segment this file.
-					echo "Please decompress the downloaded file."
-					exit 1
-				fi
-			fi
-		JD=$(awk 'BEGIN{printf("%.1f\n",'$JD+$intv/24')}')
-		done
-		if [[ $merge = 1 ]] && [[ $segment_intv -ge 24 ]]
-		then
-			Merge
-		fi
-      let j=j+1
+	  	JD=$(awk 'BEGIN{printf("%.1f\n",'$JD+$intv/24')}')
+	  	done
+	  	if [[ $merge = 1 ]] && [[ $segment_intv -ge 24 ]]
+	  	then
+	  		Merge
+	  	fi
+     	let j=j+1
 	done
 	if [[ $merge = 1 ]] && [[ $segment_intv -ge 24 ]]
 	then
@@ -728,7 +785,7 @@ then # When the product type is OBS and 's' displays a list file.
 	fi
    exit 1 
 else
-	# The normal mode
+	  # The normal mode
 	for ((i=0;i<intv_num;++i))
 	do
 		if [[ $product_type = "OBS" ]]
@@ -739,30 +796,32 @@ else
 		HourCode
 		URLMatch
 		Download
-		   # When the $savefile doesn't exist, jump out the loop
-		if [[ ! -e $savefile ]]
-		then
-			continue
-		fi
-		if [[ "$de" = "1" ]]
-		then 
-			Decompress
-		fi
-		if [[ $segment_intv = 24 ]]
-		then
-			FileList[i]=$savefile
-		fi
-		if [[ $product_type = "OBS" ]] && [[ $segment_intv -lt 24 ]]
-		then
-			if [ $de = 1 ]
+	   if [[ -e $zfile ]] || [[ -e $savefile ]]
+	   then
+			if [[ "$de" = "1" ]]
+			then 
+				Decompress
+			fi
+			if [[ $segment_intv = 24 ]]
 			then
-				Segment 
-			else
-				# If the downloaded file has not been decompressed, we 
-				# can't segment this file.
-				echo "Please decompress the downloaded file first."
-				exit 1
-		    fi
+				FileList[i]=$savefile
+			fi
+			if [[ $product_type = "OBS" ]] && [[ $segment_intv -lt 24 ]]
+			then
+				if [ $de = 1 ]
+				then
+					Segment 
+				else
+					  # If the downloaded file has not been decompressed, we 
+					  # can't segment this file.
+					echo "Please decompress the downloaded file first."
+					exit 1
+				fi
+			fi
+	   else
+				# When the $savefile doesn't exist, jump out the loop
+			JD=$(awk 'BEGIN{printf("%.1f\n",'$JD+$intv/24')}')
+			continue
 		fi
 		JD=$(awk 'BEGIN{printf("%.1f\n",'$JD+$intv/24')}')
 	done
@@ -777,3 +836,7 @@ else
 		MakeList
 	fi
 fi
+
+# =========================================================
+# The End
+# =========================================================
