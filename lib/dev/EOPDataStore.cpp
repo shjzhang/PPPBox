c90d9c644afd900f2903bac4765501d4a47db1c9
hwei <whusgghwei@hweimatoMacBook-Pro.local>
2016-03-24 20:39:06 +0800

    *header.cpp&hpp changed for rinex2.20   EOPDataStore.cpp change for EOP file read

--- lib/dev/EOPDataStore.cpp
+++ lib/dev/EOPDataStore.cpp
@@ -133,36 +133,6 @@ namespace gpstk
 
       clear();
 
-<<<<<<< HEAD
-      // first we skip the header section
-      // skip the header
-
-      //version 2
-      //EOP  SOLUTION
-      //  MJD         X        Y     UT1-UTC    LOD   Xsig   Ysig   UTsig LODsig  Nr Nf Nt     Xrt    Yrt  Xrtsig Yrtsig   dpsi    deps
-      //               10**-6"        .1us    .1us/d    10**-6"     .1us  .1us/d                10**-6"/d    10**-6"/d        10**-6
-
-//      string temp;
-//      getline(inpf,temp);	
-//      getline(inpf,temp);  
-//      getline(inpf,temp);  
-      string temp,tempm;
-      char tmpc[9],tmp[9];
-//        read eop file untils line show "MJD" in the first 8 char
-      do
-      {
-      getline(inpf,temp);
-      tempm=temp.substr(0,8);
-      strcpy(tmpc,tempm.c_str());
-      sscanf(tmpc,"%s",tmp);
-//      cout<<tmp<<endl;
-      }
-      // consider "mjd" or "MJD"
-      while((strcmp(tmp, "MJD") != 0)&&(strcmp(tmp, "MJD") != 0));
-//      skip the line show units
-      getline(inpf,temp);
-      
-=======
       // version 2
       // EOP  SOLUTION
       //   MJD         X        Y     UT1-UTC    LOD   ....
@@ -180,7 +150,6 @@ namespace gpstk
          // pass the second line of the data
       getline(inpf,temp);  
 
->>>>>>> dev
       bool ok (true);
       while(!inpf.eof() && inpf.good()) 
       {
