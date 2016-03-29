4621d09958f5ced4533facb5c29a25e1e163ae3f
shjzhangwhu <shjzhang@sgg.whu.edu.cn>
2016-03-29 15:52:12 +0800

    add pppar_example.sh to examples/

--- examples/ppp_example.sh
+++ examples/ppp_example.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
-rm ppp_example.rnxlist
-echo "./cagz2820.11o" >> ppp_example.rnxlist
-echo "./cagz2830.11o" >> ppp_example.rnxlist
-echo "./cagz2820.11o" >> ppp_example.rnxlist
+> ppp_example.rnxlist
+echo "./cagz2831.11o" >> ppp_example.rnxlist
+echo "./cagz2832.11o" >> ppp_example.rnxlist
+echo "./cagz2833.11o" >> ppp_example.rnxlist
+echo "./cagz2834.11o" >> ppp_example.rnxlist
 
-rm ppp_example.ephlist
+echo "./brus2871.11o" >> ppp_example.rnxlist
+echo "./brus2872.11o" >> ppp_example.rnxlist
+echo "./brus2873.11o" >> ppp_example.rnxlist
+echo "./brus2874.11o" >> ppp_example.rnxlist
+
+
+> ppp_example.ephlist
 echo "./igs16566.sp3" >> ppp_example.ephlist
 echo "./igs16570.sp3" >> ppp_example.ephlist
 echo "./igs16571.sp3" >> ppp_example.ephlist
@@ -15,7 +22,7 @@ echo "./igs16574.sp3" >> ppp_example.ephlist
 echo "./igs16575.sp3" >> ppp_example.ephlist
 echo "./igs16576.sp3" >> ppp_example.ephlist
 
-rm ppp_example.clklist
+> ppp_example.clklist
 echo "./igs16566.clk_30s" >> ppp_example.clklist
 echo "./igs16570.clk_30s" >> ppp_example.clklist
 echo "./igs16571.clk_30s" >> ppp_example.clklist
@@ -25,12 +32,12 @@ echo "./igs16574.clk_30s" >> ppp_example.clklist
 echo "./igs16575.clk_30s" >> ppp_example.clklist
 echo "./igs16576.clk_30s" >> ppp_example.clklist
 
-rm ppp_example.erplist
+> ppp_example.erplist
 echo "./igs16567.erp" >> ppp_example.erplist
 echo "./igs16577.erp" >> ppp_example.erplist
 
 
-rm msc.txt
+> msc.txt
 # convert ssc2msc
 ssc2msc -s igs11P1656.ssc 
 ssc2msc -s igs11P1657.ssc 
@@ -40,12 +47,17 @@ cat igs1656.msc >> msc.txt
 cat igs1657.msc >> msc.txt
 cat igs1658.msc >> msc.txt
 
-rm ppp_example.outlist
-echo "./cagz2820.11o.out" >> ppp_example.outlist
-echo "./cagz2830.11o.out" >> ppp_example.outlist
-echo "./cagz2840.11o.out" >> ppp_example.outlist
+> ppp_example.outlist
+echo "./cagz2831.11o.out" >> ppp_example.outlist
+echo "./cagz2832.11o.out" >> ppp_example.outlist
+echo "./cagz2833.11o.out" >> ppp_example.outlist
+echo "./cagz2834.11o.out" >> ppp_example.outlist
+
+echo "./brus2871.11o.out" >> ppp_example.outlist
+echo "./brus2872.11o.out" >> ppp_example.outlist
+echo "./brus2873.11o.out" >> ppp_example.outlist
+echo "./brus2874.11o.out" >> ppp_example.outlist
 
 # now, Let's perform the ppp positioning
 ppp -r ppp_example.rnxlist -s ppp_example.ephlist -k ppp_example.clklist -e ppp_example.erplist -m msc.txt -o ppp_example.outlist
-
 
