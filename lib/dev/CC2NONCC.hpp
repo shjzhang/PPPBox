6075cc41f35177173b6ef4c939976c07496f37c4
whu67 <xj67@whu.edu.cn>
2016-05-27 14:17:21 +0800

    Update the CC2NONCC.cpp

--- lib/dev/CC2NONCC.hpp
+++ lib/dev/CC2NONCC.hpp
@@ -38,132 +38,140 @@ namespace gpstk
 
    NEW_EXCEPTION_CLASS(RecTypeNotFound, gpstk::Exception);
 
-	/** @addtogroup DataStructures */
-	// @{
+   /** @addtogroup DataStructures */
+   // @{
    //
-	/** This class convert code observables which uses the CC(cross-correlation)
-	 * technique to pseudorange observables compatible with the modern Y-codeless
-	 * pseudorange tracking.
-	 *
-	 * Specifically, the C1 and X2(equivalent to C1+(P2-P1)) pseudorange observables
-	 * of cross-correlation receivers are replaced by
-	 *   C1 --> C1 + c*p1c1DCB
-	 *   X2 --> X2 + c*p1c1DCB  
-	 * here 'c' means light velocity, "p1c1DCB" is code bias between P1 and C1 which
-	 * published by CODE monthly.
-	 *
-	 * In addition, some non-cross-correlator receivers provides the C1 observable
-	 * rather than P1. In this condition, this class will replace C1 by
-	 *  C1 --> C1 + c*p1c1DCB
-	 *
-	 * A typical way to use this class follows:
-	 *
-	 * @code
+   /** This class convert code observables which uses the CC(cross-correlation)
+    * technique to pseudorange observables compatible with the modern Y-codeless
+    * pseudorange tracking.
     *
-	 * gnssRinex gRin;
-	 * CC2NONCC cc2noncc;
-	 * cc2noncc.setRecType("TRIMBLE 4000SSE");
-	 * cc2noncc.setDCBFile("P1C11001.DCB");
-	 * cc2noncc.setRecTypeFile("receiver_bernese.lis");
-	 *
-	 * while(rin >> gRin)
-	 * {
-	 *	   gRin >> cc2noncc;
-	 * }
+    * Specifically, the C1 and X2(equivalent to C1+(P2-P1)) pseudorange observables
+    * of cross-correlation receivers are replaced by
+    *   C1 --> C1 + c*p1c1DCB
+    *   X2 --> X2 + c*p1c1DCB  
+    * here 'c' means light velocity, "p1c1DCB" is code bias between P1 and C1 which
+    * published by CODE monthly.
     *
-	 * @endcode
+    * In addition, some non-cross-correlator receivers provides the C1 observable
+    * rather than P1. In this condition, this class will replace C1 by
+    *  C1 --> C1 + c*p1c1DCB
+    *
+    * A typical way to use this class follows:
+    *
+    * @code
+    *
+    * gnssRinex gRin;
+    * CC2NONCC cc2noncc;
+    * cc2noncc.setRecType("TRIMBLE 4000SSE");
+    * cc2noncc.loadDCBFile("P1C11001.DCB");
+    * cc2noncc.loadRecTypeFile("receiver_bernese.lis");
+    *
+    * while(rin >> gRin)
+    * {
+    *	   gRin >> cc2noncc;
+    * }
+    *
+    * @endcode
     *
     * @sa 
     *
-	 */
-	class CC2NONCC : public ProcessingClass
-	{
-	   public :
+    */
+   class CC2NONCC : public ProcessingClass
+   {
+      public :
 
-	        // Default constructr
-	      CC2NONCC()
+           // Default constructr
+         CC2NONCC()
             :copyC1ToP1(false)                          
          {};
 
-	      
-	        /** Sets type of receiver
-	         * @param  rectype    Type of receiver
-	         */
-	      virtual CC2NONCC& setRecType(const std::string& rectype);
+           /** Common constructor
+            *
+            * @param dcbDataReader    object to read and store DCB data
+            */
+         CC2NONCC( DCBDataReader& dcbDataReader )
+            : dcbP1C1(&dcbDataReader) {};
+         
 
-	        /** Sets name of file containing DCBs data.
-	         * @param  fileP1C1    Name of the file containing DCB(P1-C1)
-	         */
-	      virtual CC2NONCC& setDCBFile(const std::string& fileP1C1)
-	      	throw(FileMissingException);
+           /** Set type of receiver
+            *
+            * @param  rectype    Type of receiver
+            */
+         virtual CC2NONCC& setRecType(const std::string& rectype);
 
-	        /** Sets whether to create P1 .
-	         * @param copy    boolean to indiate whether copy P1
-	         */
-	      virtual CC2NONCC& setCopyC1ToP1(bool copy)
+
+           /** Add the file containing receiver type information
+         	*
+            * @param  recfile    Name of the file containing receiver information
+            */
+         virtual CC2NONCC& loadRecTypeFile(const std::string& recfile);
+
+
+           /** Set whether to create P1 .
+         	*
+            * @param copy    boolean to indiate whether copy P1
+            */
+         virtual CC2NONCC& setCopyC1ToP1(bool copy)
          { copyC1ToP1 = copy; return (*this); }
+           
 
-	        
-	        /** Sets name of file containing DCBs data.
-	         * @param  recfile    Name of the file containing receiver information
-	         * observable type(C1/P2, C1/X2, or P1/P2)
-	         */
-	      virtual CC2NONCC& setRecTypeFile(const std::string& recfile);
-
-	        /** Returns a satTypeValueMap object, adding the new data generated
-	         *  when calling this object.
-	         *
-	         * @param  time     Epoch corresponding to the data.
-	         * @param  gData    Data object holding the data.
-	         */
-	      virtual satTypeValueMap& Process( const CommonTime& time,
-	      		                            satTypeValueMap& gData)
-	      	throw(ProcessingException);
-
-	        /** Returns a gnssSatTypeValue object, adding the new data
-	         *  generated when calling this object.
-	         *
-	         * @param gData     Data object holding the data.
-	         */
-	      virtual gnssSatTypeValue& Process(gnssSatTypeValue& gData)
-	      	throw(ProcessingException)
-	      { Process(gData.header.epoch, gData.body); return gData; };
-
-	        /** Returns a gnssRinex object, adding the new data generated
-	         *  when calling this object.
-	         *
-	         * @param gData     Data object holding the data.
-	         */
-	      virtual gnssRinex& Process(gnssRinex& gData)
-	      	throw(ProcessingException)
-	      { Process(gData.header.epoch, gData.body); return gData; };
+           /** Return a satTypeValueMap object, adding the new data generated
+            *  when calling this object.
+            *
+            * @param  time     Epoch corresponding to the data.
+            * @param  gData    Data object holding the data.
+            */
+         virtual satTypeValueMap& Process( const CommonTime& time,
+                                           satTypeValueMap& gData)
+            throw(ProcessingException);
 
 
-	        // Returns a string identifying this object.
-	      virtual std::string getClassName () const;
+           /** Returns a gnssSatTypeValue object, adding the new data
+            *  generated when calling this object.
+            *
+            * @param gData     Data object holding the data.
+            */
+         virtual gnssSatTypeValue& Process(gnssSatTypeValue& gData)
+            throw(ProcessingException)
+         { Process(gData.header.epoch, gData.body); return gData; };
 
 
-	        // Default deconstructr
-	      ~CC2NONCC(){};
+           /** Returns a gnssRinex object, adding the new data generated
+            *  when calling this object.
+            *
+            * @param gData     Data object holding the data.
+            */
+         virtual gnssRinex& Process(gnssRinex& gData)
+            throw(ProcessingException)
+         { Process(gData.header.epoch, gData.body); return gData; };
 
 
-	   private:
+           // Returns a string identifying this object.
+         virtual std::string getClassName () const;
 
-			  // The choice if copy the C1 to P1,if it's true,we will
-			  // copy C1 to P1
+
+           // Default deconstructr
+         ~CC2NONCC(){};
+
+
+      private:
+
+           // The choice if copy the C1 to P1,if it's true,we will
+           // copy C1 to P1
          bool copyC1ToP1;
 
-	        // Object to acess DCB data from CODE
-	      DCBDataReader dcbP1C1;
+           // Object that reads and stores DCB data
+         DCBDataReader *dcbP1C1;
 
-	        // Object to acess receiver code observable type
-	      RecTypeDataReader recTypeData;
+           // Object to acess receiver code observable type
+         RecTypeDataReader recTypeData;
 
-	      std::string recType;
-	      
-	}; // End of class 'CC2NONCC'
+           // The type of receiver
+         std::string recType;
+         
+   }; // End of class 'CC2NONCC'
 
-	   // @}
+      // @}
 
 }; // end of namespace gpstk
 
