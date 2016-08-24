2e7d7188782fdfc44a1a9398bd0e153d1b139c37
WeiWangWhu <1058604350@qq.com>
2016-08-24 22:09:04 +0800

    update

--- workplace/pppgnss/pppgnss.sh
+++ workplace/pppgnss/pppgnss.sh
@@ -1,59 +1,51 @@
 #!/bin/bash
 
 rm rnxlist
-echo "./gmsd0320.16o" >> rnxlist
-echo "./nnor0320.16o" >> rnxlist
-echo "./mal20320.16o" >> rnxlist
-echo "./mayg0320.16o" >> rnxlist
-echo "./jfng0320.16o" >> rnxlist
-echo "./ohi30320.16o" >> rnxlist
-echo "./unb30320.16o" >> rnxlist
-echo "./kour0320.16o" >> rnxlist
-echo "./wtzr0320.16o" >> rnxlist
+echo "./gmsd0370.16o" >> rnxlist
+echo "./cut00370.16o" >> rnxlist
+echo "./nnor0370.16o" >> rnxlist
+echo "./jfng0370.16o" >> rnxlist
+
 
 
 rm ephlist
-echo "./gbm18820.sp3" >> ephlist
-echo "./gbm18821.sp3" >> ephlist
-echo "./gbm18822.sp3" >> ephlist
-
+echo "./gbm18825.sp3" >> ephlist
+echo "./gbm18826.sp3" >> ephlist
+echo "./gbm18830.sp3" >> ephlist
 
 
 rm clklist
-echo "./gbm18820.clk" >> clklist
-echo "./gbm18821.clk" >> clklist
-echo "./gbm18822.clk" >> clklist
+echo "./gbm18825.clk" >> clklist
+echo "./gbm18826.clk" >> clklist
+echo "./gbm18830.clk" >> clklist
 
 
 rm navlist
-echo "./brdc0320.16g" >> navlist
+echo "./brdc0370.16g" >> navlist
 
 rm erplist
 echo "./igs18827.erp" >> erplist
+echo "./igs18837.erp" >> erplist
+
 
 rm dcblist
 echo "./P1C11602_RINEX.DCB" >> dcblist
 
 
 rm outlist
-echo "./result/gmsd0320.16o.out.grce.kine" >> outlist
-echo "./result/nnor0320.16o.out.grce.kine" >> outlist
-echo "./result/mal20320.16o.out.grce.kine" >> outlist
-echo "./result/mayg0320.16o.out.grce.kine" >> outlist
-echo "./result/jfng0320.16o.out.grce.kine" >> outlist
-echo "./result/ohi30320.16o.out.grce.kine" >> outlist
-echo "./result/unb30320.16o.out.grce.kine" >> outlist
-echo "./result/kour0320.16o.out.grce.kine" >> outlist
-echo "./result/wtzr0320.16o.out.grce.kine" >> outlist
+echo "./result/gmsd0370.16o.out.grce.kine" >> outlist
+echo "./result/cut00370.16o.out.grce.kine" >> outlist
+echo "./result/nnor0370.16o.out.grce.kine" >> outlist
+echo "./result/jfng0370.16o.out.grce.kine" >> outlist
 
 rm msc.txt
 # convert ssc2msc
- ssc2msc -s igs16P1882.ssc 
+ ssc2msc -s igs16P1887.ssc 
 
-# now, let's mertemp all the msc files togrcether for pppar positioning
- cat igs1882.msc >> msc.txt
+# now, let's meremp all the msc files  for ppp positioning
+ cat igs1887.msc >> msc.txt
 
 # now, Let's perform the ppp positioning
-pppgnss -r rnxlist -s ephlist -e erplist  -k clklist -g navlist -D dcblist -m msc.txt -o outlist
+pppgnss -r rnxlist -s ephlist -e erplist  -k clklist   -g navlist -D dcblist -m msc.txt -o outlist
 
 
