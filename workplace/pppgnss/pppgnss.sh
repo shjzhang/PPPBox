739de404cc2ed6731fed5b7dd139cb7f9568816e
WeiWangWhu <1058604350@qq.com>
2016-11-15 15:48:33 +0800

    delete some files

--- workplace/pppgnss/pppgnss.sh
+++ workplace/pppgnss/pppgnss.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+mypath="./result"
+
+if [ ! -d "$mypath" ];then
+	mkdir $mypath
+fi
+
 rm rnxlist
 echo "./gmsd0370.16o" >> rnxlist
-echo "./cut00370.16o" >> rnxlist
-echo "./nnor0370.16o" >> rnxlist
 echo "./jfng0370.16o" >> rnxlist
-
+echo "./nnor0370.16o" >> rnxlist
 
 
 rm ephlist
@@ -19,9 +23,8 @@ echo "./gbm18825.clk" >> clklist
 echo "./gbm18826.clk" >> clklist
 echo "./gbm18830.clk" >> clklist
 
-
 rm navlist
-echo "./brdc0370.16g" >> navlist
+echo "./brdc0370.16g" >> navlist          
 
 rm erplist
 echo "./igs18827.erp" >> erplist
@@ -31,21 +34,18 @@ echo "./igs18837.erp" >> erplist
 rm dcblist
 echo "./P1C11602_RINEX.DCB" >> dcblist
 
-
 rm outlist
-echo "./result/gmsd0370.16o.out.grce.kine" >> outlist
-echo "./result/cut00370.16o.out.grce.kine" >> outlist
-echo "./result/nnor0370.16o.out.grce.kine" >> outlist
-echo "./result/jfng0370.16o.out.grce.kine" >> outlist
+echo "$mypath/gmsd0370.out.grce.kine" >> outlist
+echo "$mypath/jfng0370.out.grce.kine" >> outlist
+echo "$mypath/nnor0370.out.grce.kine" >> outlist
 
 rm msc.txt
 # convert ssc2msc
- ssc2msc -s igs16P1887.ssc 
+ ssc2msc -s igs16P1882.ssc 
 
-# now, let's meremp all the msc files  for ppp positioning
- cat igs1887.msc >> msc.txt
+# now, let's merge all the msc files  for ppp positioning
+ cat igs1882.msc >> msc.txt
 
 # now, Let's perform the ppp positioning
-pppgnss -r rnxlist -s ephlist -e erplist  -k clklist   -g navlist -D dcblist -m msc.txt -o outlist
-
+pppgnss -r rnxlist -s ephlist -e erplist  -k clklist  -g navlist -D dcblist -m msc.txt -o outlist
 
