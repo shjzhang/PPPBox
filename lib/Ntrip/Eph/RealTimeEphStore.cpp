9a3366e426fdd16852b9e967947401f575c62f40
whu67 <xj67@whu.edu.cn>
2017-03-06 05:02:15 -0800

    Add the RealTimeEphStore class

---
+++ lib/Ntrip/Eph/RealTimeEphStore.cpp
@@ -0,0 +1,333 @@
+#include "RealTimeEphStore.hpp"
+#include "StringUtils.hpp"
+
+namespace gpstk
+{
+    RealTimeEphStore::RealTimeEphStore()
+    {
+        m_bOnlyHealthy = true;
+        m_bUseCorrection = false;
+    }
+
+    RealTimeEphStore::RealTimeEphStore(RealTimeEphStore &right)
+    {
+        m_bOnlyHealthy = right.m_bOnlyHealthy;
+        m_bUseCorrection = right.m_bUseCorrection;
+        std::map<SatID, std::deque<OrbitEph2*> >::iterator it = right.m_ephMap.begin();
+        while(it!=right.m_ephMap.end())
+        {
+            std::deque<OrbitEph2*>& q = it->second;
+            std::deque<OrbitEph2*> eq;
+            for(unsigned i = 0;i<q.size();++i)
+            {
+                eq.push_back(q[i]);
+            }
+            m_ephMap[it->first] = eq;
+            ++it;
+        }
+    }
+
+    RealTimeEphStore::~RealTimeEphStore()
+    {
+        clear();
+    }
+
+    void RealTimeEphStore::clear()
+    {
+        std::map<SatID, std::deque<OrbitEph2*> >::iterator it = m_ephMap.begin();
+        while(it!=m_ephMap.end())
+        {
+            std::deque<OrbitEph2*>& q = it->second;
+            for(unsigned i = 0;i<q.size();++i)
+            {
+                delete q[i];
+            }
+            ++it;
+        }
+    }
+
+    Xvt RealTimeEphStore::getXvt(const SatID& sat, const CommonTime& t) const
+    {
+        try
+        {
+            // get the appropriate OrbitEph
+            const OrbitEph2* eLast = this->ephLast(sat);
+            const OrbitEph2* ePrev = this->ephPrev(sat);
+
+            // compute the position, velocity and time
+            Xvt sv;
+            if( eLast && eLast->getCrd(t,sv,m_bUseCorrection) )
+            {
+                return sv;
+            }
+            else if( ePrev && ePrev->getCrd(t,sv,m_bUseCorrection) )
+            {
+                return sv;
+            }
+            if(!eLast || !ePrev)
+            {
+               GPSTK_THROW(InvalidRequest("No OrbitEph for satellite "
+                                         + StringUtils::asString(sat)));
+            }
+
+            // no consideration is given to health here (OrbitEph does not have health);
+            // derived classes should override isHealthy()
+            //if(m_bOnlyHealthy)
+            //   GPSTK_THROW(InvalidRequest("Not healthy"));
+
+
+            return sv;
+        }
+        catch(InvalidRequest& ir)
+        {
+            GPSTK_RETHROW(ir);
+        }
+    }
+
+    bool RealTimeEphStore::putNewEph(OrbitEph2 *eph, bool check)
+    {
+        std::lock_guard<std::mutex> guard(m_mutex);
+
+        if(eph == 0)
+        {
+            return false;
+        }
+        eph->dataLoadedFlag = true;
+        if(check)
+        {
+            checkEphmeris(eph);
+        }
+        const GPSEphemeris2* ephGPS = dynamic_cast<const GPSEphemeris2*>(eph);
+
+        OrbitEph2* ephNew = 0;
+
+        if(ephGPS)
+        {
+            ephNew = new GPSEphemeris2(*ephGPS);
+        }
+        else
+        {
+            return false;
+        }
+
+        const OrbitEph2* ephOld = ephLast(eph->satID);
+
+        if(ephOld && (ephOld->getCheckState() == OrbitEph2::bad
+                   || ephOld->getCheckState() == OrbitEph2::outdated))
+        {
+            ephOld = 0;
+        }
+
+        if((ephOld ==0 || ephNew->isNewerThan(ephOld)) &&
+           (eph->getCheckState() != OrbitEph2::bad &&
+            eph->getCheckState() != OrbitEph2::outdated))
+        {
+            std::deque<OrbitEph2*>& qq = m_ephMap[eph->satID];
+            qq.push_back(ephNew);
+            if(qq.size() > m_iMaxQueueSize)
+            {
+                delete qq.front();
+                qq.pop_front();
+            }
+            return true;
+        }
+        else
+        {
+            delete ephNew;
+            return false;
+        }
+    }
+
+    void RealTimeEphStore::checkEphmeris(OrbitEph2 *eph)
+    {
+        if(!eph)
+        {
+            return;
+        }
+
+        // Simple Check - check satellite radial distance
+        // ----------------------------------------------
+        Xvt sv;
+
+        if(!eph->getCrd(eph->ctToc, sv, false))
+        {
+            eph->setCheckState(OrbitEph2::bad);
+            return;
+        }
+
+        double rr = sv.x.mag();
+
+        const double MINDIST = 2.e7;
+        const double MAXDIST = 6.e7;
+
+        if (rr < MINDIST || rr > MAXDIST)
+        {
+            eph->setCheckState(OrbitEph2::bad);
+            return;
+        }
+
+        // Check whether the epoch is too far away from the current time
+        // --------------------------------------------------------
+
+        CommonTime toc = eph->ctToc;
+        SystemTime now;
+        CommonTime currentTime(now);
+
+        currentTime.setTimeSystem(toc.getTimeSystem());
+        double timeDiff = fabs(toc - currentTime);
+        SatID::SatelliteSystem system = eph->satID.system;
+        if(system == SatID::systemGPS || system == SatID::systemGalileo)
+        {
+            // update interval: 2h, data sets are valid for 4 hours
+            if(timeDiff > 4*3600)
+            {
+                // outdated
+                eph->setCheckState(OrbitEph2::outdated);
+                return;
+            }
+        }
+        else if(system == SatID::systemGlonass)
+        {
+            // updated every 30 minutes
+            if(timeDiff > 1*3600)
+            {
+                // outdated
+                eph->setCheckState(OrbitEph2::outdated);
+                return;
+            }
+        }
+        else if(system == SatID::systemBeiDou)
+        {
+            // updates 1 (GEO) up to 6 hours
+            if(timeDiff > 6*3600)
+            {
+                // outdated
+                eph->setCheckState(OrbitEph2::outdated);
+                return;
+            }
+        }
+        else if(system == SatID::systemQZSS)
+        {
+            // orbit parameters are valid for 7200 seconds (at minimum)
+            if(timeDiff > 4*3600)
+            {
+                // outdated
+                eph->setCheckState(OrbitEph2::outdated);
+                return;
+            }
+        }
+
+        // Check consistency with older ephemerides
+        // ----------------------------------------
+        const double MAXDIFF = 1000.0;
+        OrbitEph2* ephL = (OrbitEph2*)ephLast(eph->satID);
+
+        if(ephL)
+        {
+            Xvt svL;
+            ephL->getCrd(eph->ctToc, svL, false);
+
+            double dt = eph->ctToc - ephL->ctToc;
+            double diffX = (sv.x - svL.x).mag();
+            double diffC = fabs(sv.clkbias - svL.clkbias) * C_MPS;
+
+            // some lines to allow update of ephemeris data sets after outage
+            if(system == SatID::systemGPS && dt > 4*3600)
+            {
+                ephL->setCheckState(OrbitEph2::outdated);
+                return;
+            }
+            if(system == SatID::systemGalileo && dt > 4*3600)
+            {
+                ephL->setCheckState(OrbitEph2::outdated);
+                return;
+            }
+            if(system == SatID::systemGlonass && dt > 1*3600)
+            {
+                ephL->setCheckState(OrbitEph2::outdated);
+                return;
+            }
+            if(system == SatID::systemBeiDou && dt > 6*3600)
+            {
+                ephL->setCheckState(OrbitEph2::outdated);
+                return;
+            }
+            if(system == SatID::systemQZSS && dt > 4*3600)
+            {
+                ephL->setCheckState(OrbitEph2::outdated);
+                return;
+            }
+
+            if (diffX < MAXDIFF && diffC < MAXDIFF)
+            {
+              if (dt != 0.0) {
+                eph->setCheckState(OrbitEph2::ok);
+                ephL->setCheckState(OrbitEph2::ok);
+              }
+              else
+              {
+                  // do nothing here
+              }
+            }
+            else
+            {
+              if (ephL->getCheckState() == OrbitEph2::ok)
+              {
+                eph->setCheckState(OrbitEph2::bad);
+              }
+              else
+              {
+                  // do nothing here
+              }
+            }
+        }
+    }
+
+    const OrbitEph2* RealTimeEphStore::ephLast(const SatID &sat) const
+    {
+        std::map<SatID, std::deque<OrbitEph2*> >::const_iterator it;
+        it = m_ephMap.find(sat);
+        if(it != m_ephMap.end())
+        {
+            return it->second.back();
+        }
+        else
+        {
+            return 0;
+        }
+    }
+
+    const OrbitEph2 *RealTimeEphStore::ephPrev(const SatID &sat) const
+    {
+        std::map<SatID, std::deque<OrbitEph2*> >::const_iterator it;
+        it = m_ephMap.find(sat);
+        if(it != m_ephMap.end())
+        {
+            unsigned n = it->second.size();
+            if(n > 1)
+            {
+                return it->second.at(n-2);
+            }
+            else
+            {
+                // do nothing here
+            }
+        }
+        else
+        {
+            return 0;
+        }
+    }
+
+    std::list<SatID> RealTimeEphStore::getSatList()
+    {
+        std::list<SatID> satList;
+        std::map<SatID, std::deque<OrbitEph2*> >::iterator it;
+        for(it=m_ephMap.begin();it!=m_ephMap.end();++it)
+        {
+            satList.push_back(it->first);
+        }
+        return satList;
+    }
+
+}
---
+++ lib/Ntrip/Eph/RealTimeEphStore.hpp
@@ -0,0 +1,112 @@
+#ifndef GPSTK_REALTIMEEPHSTORE_HPP
+#define GPSTK_REALTIMEEPHSTORE_HPP
+
+#include <iostream>
+#include <list>
+#include <deque>
+#include <map>
+#include <mutex>
+
+#include "OrbitEph2.hpp"
+#include "GPSEphemeris2.hpp"
+#include "Exception.hpp"
+#include "SatID.hpp"
+#include "CommonTime.hpp"
+#include "XvtStore.hpp"
+namespace gpstk
+{
+    class RealTimeEphStore : public XvtStore<SatID>
+    {
+    public:
+
+        /// Constructor
+        RealTimeEphStore();
+
+        /// Copy constructor
+        RealTimeEphStore(RealTimeEphStore& right);
+
+        /// Destructor
+        virtual ~RealTimeEphStore();
+
+        /// Returns the position, velocity, and clock offset of the indicated
+        /// object in ECEF coordinates (meters) at the indicated time.
+        /// @param[in] id satellite SatID
+        /// @param[in] t the time to look up
+        /// @return the Xvt of the satellite at the indicated time
+        /// @throw InvalidRequest if the satellite is not stored or there are no
+        ///        orbit elements at time t.
+        virtual Xvt getXvt(const SatID& id, const CommonTime& t) const;
+
+
+        /// Set the choice if using correction data
+        void usingCorrection(bool flag)
+        {
+            m_bUseCorrection = flag;
+        }
+
+        /// Store new ephemeris
+        bool putNewEph(OrbitEph2* eph, bool check);
+
+        /// Get the last ephmeris in the queue
+        const OrbitEph2* ephLast(const SatID& sat) const;
+
+        /// Get the previous ephmeris in the queue
+        const OrbitEph2* ephPrev(const SatID& sat) const;
+
+        /// Get the satellite prn list
+        std::list<SatID> getSatList();
+
+        /// Clear the dataset, meaning remove all data
+        virtual void clear();
+
+        /// Return true if velocity data is present in the store
+        virtual bool hasVelocity() const
+        { return true; }
+
+        /// Return true if the given SatID is present in the store
+        virtual bool isPresent(const SatID& sat) const
+        {
+           if(m_ephMap.find(sat) != m_ephMap.end()) return true;
+           return false;
+        }
+
+        /// A debugging function that outputs in human readable form,
+        /// all data stored in this object.
+        /// @param[in] s the stream to receive the output; defaults to cout
+        /// @param[in] detail the level of detail to provide
+        virtual void dump(std::ostream& s = std::cout, short detail = 0)const {;}
+
+        /// Edit the dataset, removing data outside the indicated time interval
+        /// @param[in] tmin defines the beginning of the time interval
+        /// @param[in] tmax defines the end of the time interval
+        virtual void edit(const CommonTime& tmin,
+                          const CommonTime& tmax = CommonTime::END_OF_TIME){;}
+
+        /// Return the time system of the store
+        virtual TimeSystem getTimeSystem(void) const {return TimeSystem::Unknown;}
+
+        /// Determine the earliest time for which this object can successfully
+        /// determine the Xvt for any object.
+        /// @return The initial time
+        /// @throw InvalidRequest This is thrown if the object has no data.
+        virtual CommonTime getInitialTime(void) const { return CommonTime::BEGINNING_OF_TIME;}
+
+        /// Determine the latest time for which this object can successfully
+        /// determine the Xvt for any object.
+        /// @return The final time
+        /// @throw InvalidRequest This is thrown if the object has no data.
+        virtual CommonTime getFinalTime(void) const { return CommonTime::END_OF_TIME;}
+
+    private:
+        /// Check the ephmeris
+        void checkEphmeris(OrbitEph2* eph);
+
+        std::mutex m_mutex;                                ///< Mutex
+        static const unsigned   m_iMaxQueueSize = 5;       ///< Maximun size of ephmeris data queue
+        std::map<SatID, std::deque<OrbitEph2*> > m_ephMap; ///< Map to store the ephmeris
+        bool m_bUseCorrection;                             ///< If use the orbit and clock correction
+        bool m_bOnlyHealthy;                               ///< If only use healthy ephemeris
+    };
+
+} // End of namespace gpstk
+#endif // GPSTK_REALTIMEEPHSTORE_HPP
